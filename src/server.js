18b40c38100a93b907db3f8ffe41d599633061d2
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-10-09 15:22:20 +0100

    Add static routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+// // http://localhost:5001/
+// app.use("/", express.static("blank"));
+// // http://localhost:5001/example
+// app.use("/example", express.static("example"));
+
+app.use("/", express.static("bookHome"));
+app.use("/about", express.static("bookAbout"));
+app.use("/contact", express.static("bookContact"));
+
 app.listen(5001, () => {
   console.log("server is listening");
 });
