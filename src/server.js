2f9b0518cbdd49a5c363990a33dee92ee0224a9e
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-10-09 17:31:15 +0100

    Enhancement: routes post get server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,37 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/books", (request, response) => {
+  console.log(request.originalUrl);
+  const book = {
+    title: "lord of the rings",
+    author: "tolkein",
+    genre: "fantasy",
+  };
+  const successResponse = {
+    message: "success",
+    book: book,
+  };
+
+  response.send(successResponse);
+});
+
+app.post("/books", (request, response) => {
+  const newBook = {
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  };
+
+  const successResponse = {
+    message: "success",
+    newBook: newBook,
+  };
+  response.send(successResponse);
+});
+
 app.listen(5001, () => {
   console.log("server is listening");
 });
